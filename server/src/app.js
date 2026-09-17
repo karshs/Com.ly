e9760048859c58213150a412127d20da23bc50ad
TautCoder <singhutkarsh187@gmail.com>
2026-09-17 16:39:00 +0530

    chore: initialize server project with Express and core dependencies

---
+++ server/src/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const app =  express();
+
+app.use(cors({
+
+    origin : process.env.CLIENT_URL || 'http://localhost:5173' ,
+    credentials : true,
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({extended : true})); 
+
+app.use(cookieParser());
+
+app.get('/health', (req,res) => {
+
+    res.status(200).json({
+        status : "ok",
+        timestamp : new Date().toISOString(),
+        service : 'Com.ly Backedn API'
+    });
+});
+
+module.exports = app;
